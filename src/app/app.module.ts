55875e00866d86fa2e16622b1a7a5bbfb4a0efb8
Josue Palomo <josuepalomocs@gmail.com>
2023-03-30 19:41:15 -0500

    add upload image ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { heroDocumentPlusSolid } from '@ng-icons/heroicons/solid';
 
 import { AppComponent } from './app.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+import { NgIconsModule } from '@ng-icons/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UploadImageComponent],
+  imports: [BrowserModule, NgIconsModule.withIcons({ heroDocumentPlusSolid })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
